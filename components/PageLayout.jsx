2016fdc58f7effabc469820e8ffbeb6229a93a41
keralex <kerlis.aguado26@gmail.com>
2022-09-28 12:59:53 -0300

    Creating articles

---
+++ components/PageLayout.jsx
@@ -0,0 +1,11 @@
+const PageLayout = ({children}) => {
+    return (
+        <div>
+            <main>
+                titulo
+                {children}
+            </main>
+        </div>
+    )
+}
+export default PageLayout;
